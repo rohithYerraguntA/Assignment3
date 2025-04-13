62b3637d4a67266ed29793f7e11da86bf2665631
rohith_yerragunta <rohith.yerragunta@dcmail.ca>
2025-04-13 13:42:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-# app.py
-
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Yerragunta ECS Container"
+    return "Hello from Yerragunta Cloud Run Container!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use 8080 if set by Cloud Run, fallback to 5000
+    app.run(host='0.0.0.0', port=port)
